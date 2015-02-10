45027f8a9f0fee4fec4ccf4c36a29524b0be47c3
Steven Vandevelde <icid.asset@gmail.com>
2015-02-10 22:24:04 +0100

    Refactor components

--- assets/javascripts/components/container.js
+++ assets/javascripts/components/container.js
@@ -4,41 +4,34 @@ import _ from "../vendor/underscore";
 skate("container", {
 
   created: function(el) {
-    this.instance = new Container(el);
+    el.render = _.bind(el.render, el);
+    el.bind_events();
   },
 
   detached: function(el) {
-    this.instance.unbind_events();
-    this.instance = null;
+    el.unbind_events();
+  },
+
+  prototype: {
+
+
+    bind_events() {
+      App.stateNotifier.on("change:route_page_path", this.render);
+    },
+
+    unbind_events() {
+      App.stateNotifier.off("change:route_page_path", this.render);
+    },
+
+    render(route_page_path) {
+      var page_data = App.helpers.traverse_object(route_page_path, App.data.pages);
+      var template = App.helpers.get_template("pages/" + route_page_path);
+      var compiled_template = template(page_data);
+
+      this.innerHTML = compiled_template;
+    }
+
+
   }
 
 });
-
-
-
-class Container {
-
-  constructor(el) {
-    this.el = el;
-    this.render = _.bind(this.render, this);
-
-    this.bind_events();
-  }
-
-  bind_events() {
-    App.stateNotifier.on("change:route_page_path", this.render);
-  }
-
-  unbind_events() {
-    App.stateNotifier.off("change:route_page_path", this.render);
-  }
-
-  render(route_page_path) {
-    var page_data = App.helpers.traverse_object(route_page_path, App.data.pages);
-    var template = App.helpers.get_template("pages/" + route_page_path);
-    var compiled_template = template(page_data);
-
-    this.el.innerHTML = compiled_template;
-  }
-
-}
--- assets/javascripts/components/header.js
+++ assets/javascripts/components/header.js
@@ -4,41 +4,34 @@ import _ from "../vendor/underscore";
 skate("header", {
 
   created: function(el) {
-    this.instance = new Header(el);
+    el.render = _.bind(el.render, el);
+    el.bind_events();
   },
 
   detached: function(el) {
-    this.instance.unbind_events();
-    this.instance = null;
+    el.unbind_events();
+  },
+
+  prototype: {
+
+
+    bind_events() {
+      App.stateNotifier.on("change:route_page_path", this.render);
+    },
+
+    unbind_events() {
+      App.stateNotifier.off("change:route_page_path", this.render);
+    },
+
+    render(route_page_path) {
+      var page_data = App.helpers.traverse_object(route_page_path, App.data.pages);
+      var data_object = _.extend({ _all: App.data }, page_data);
+      var compiled_template = App.templates.partials.header(data_object);
+
+      this.innerHTML = compiled_template;
+    }
+
+
   }
 
 });
-
-
-
-class Header {
-
-  constructor(el) {
-    this.el = el;
-    this.render = _.bind(this.render, this);
-
-    this.bind_events();
-  }
-
-  bind_events() {
-    App.stateNotifier.on("change:route_page_path", this.render);
-  }
-
-  unbind_events() {
-    App.stateNotifier.off("change:route_page_path", this.render);
-  }
-
-  render(route_page_path) {
-    var page_data = App.helpers.traverse_object(route_page_path, App.data.pages);
-    var data_object = _.extend({ _all: App.data }, page_data);
-    var compiled_template = App.templates.partials.header(data_object);
-
-    this.el.innerHTML = compiled_template;
-  }
-
-}
